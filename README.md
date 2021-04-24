aba93ba42a0e6c7b260be11d055f18cb3662c2f5
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-04-24 17:22:05 +0200

    #1: improved system tests to create required information for connection

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ![build status](https://github.com/jakobbeckmann/luakube/workflows/test/badge.svg)
 
-Lua abstraction of Kubernetes API.
+LuaKube is a simple client library to access the Kubernetes API. It does not abstract much from the
+API, allowing for full control, but provides some convenience functions for quick scripting.
 
 ## Testing
 
@@ -12,8 +13,14 @@ Testing is done with `busted`:
 busted --exclude-tags=system spec
 ```
 
+### System Tests
+
+The system tests require `k3d` to be installed, and the `docker` service to be running.
+
 To run the system tests:
 
 ```bash
 busted --defer-print -t system spec
+# or combined with unit tests
+busted --defer-print spec
 ```
--- spec/utils.lua
+++ spec/utils.lua
@@ -7,6 +7,13 @@ Description:
 ]]--
 
 
+local function run(cmd)
+  local fh = io.popen(cmd, "r")
+  local ctnt = fh:read("a")
+  fh:close()
+  return ctnt
+end
+
 local utils = {}
 
 local function delete_k3d_cluster(name, logfile)
@@ -14,6 +21,16 @@ local function delete_k3d_cluster(name, logfile)
     error(string.format("failed to delete k3d cluster %s: %s", name, logfile))
   end
   assert(os.remove(logfile))
+end
+
+local function initialize_sa(user)
+  local worked = os.execute("kubectl create ns demo")
+  worked = worked and os.execute("kubectl apply -f assets/sa.yaml")
+  worked = worked and os.execute("kubectl apply -f assets/crb.yaml")
+  local secret = run('kubectl -n demo get sa/admin -o jsonpath="{.secrets[0].name}"')
+  local token = run(string.format('kubectl -n demo get secret/%s -o json | jq -r ".data.token" | base64 -d', secret))
+  worked = worked and os.execute(string.format("kubectl config set-credentials %s --token=%s", user, token))
+  return worked
 end
 
 function utils.create_k3d_cluster()
@@ -24,6 +41,8 @@ function utils.create_k3d_cluster()
   if not os.execute("kubectl cluster-info >> "..logfile) == 0 then
     error("failed to create k3d cluster for testing: "..logfile)
   end
+  local username = "admin@k3d-"..name
+  assert(initialize_sa(username))
   return name, function () delete_k3d_cluster(name, logfile) end
 end
 
