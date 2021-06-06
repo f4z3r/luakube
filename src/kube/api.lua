1e67415357fd8a883d6158fd61c17a2faf50a953
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-06-06 16:21:38 +0200

    extended core V1 API, improved function generation, added progress log to README

--- src/kube/api.lua
+++ src/kube/api.lua
@@ -6,6 +6,38 @@ local json = require "json"
 
 local core_v1 = require "kube.api.core_v1"
 
+-- TODO(@jakob): cleanup query encoding functions
+local function encode(str)
+  return (str:gsub("([^A-Za-z0-9%_%.%-%~])", function(v)
+      return string.upper(string.format("%%%02x", string.byte(v)))
+  end))
+end
+
+local function replace_space_encoding(str)
+  str = encode(str)
+  return str:gsub('%%20', '+')
+end
+
+local function build_query(data)
+  local query = {}
+  local sep = '&'
+  local keys = {}
+  for k in pairs(data) do
+    keys[#keys+1] = k
+  end
+  table.sort(keys)
+  for _,name in ipairs(keys) do
+    name = encode(tostring(name))
+    local value = replace_space_encoding(tostring(data[name]))
+    if value ~= "" then
+      query[#query+1] = string.format('%s=%s', name, value)
+    else
+      query[#query+1] = name
+    end
+  end
+  return table.concat(query, sep)
+end
+
 local api = {}
 
 api.Client = {}
@@ -22,8 +54,12 @@ function api.Client:new(config)
 end
 
 -- Perform a raw API call. This returns a string of the body of the response.
-function api.Client:raw_call(method, path, body)
+function api.Client:raw_call(method, path, body, query)
   local url = self.url_ .. "/api/" .. path
+  if query then
+    local query_str = build_query(query)
+    url = url .. '?' .. query_str
+  end
   local source
   if body then
     source = ltn12.source.string(body)
@@ -46,8 +82,8 @@ function api.Client:raw_call(method, path, body)
 end
 
 -- Perform a raw API call which returns a table structure of the response.
-function api.Client:call(method, path, body)
-  local resp, err_msg, code = self:raw_call(method, path, body)
+function api.Client:call(method, path, body, query)
+  local resp, err_msg, code = self:raw_call(method, path, body, query)
   if not resp then
     error("Code "..code..": "..(err_msg or "unknown error"))
   end
--- src/kube/api/core_v1.lua
+++ src/kube/api/core_v1.lua
@@ -8,39 +8,36 @@ Description:
 
 local fun = require "fun"
 local obj = require "kube.api.objects"
+local utils = require "kube.api.utils"
 
 local core_v1 = {}
 
 core_v1.Client = {}
 
-function core_v1.Client:new(o)
-  assert(o, "API clients need to be created with a superclient")
-  local client = {}
-  self.__index = self
-  setmetatable(client, self)
-  client.client_ = o
-  client.api_ = "v1/"
-  return client
-end
+core_v1.Client.new = utils.create_client("v1/")
+core_v1.Client.call = utils.create_call()
+core_v1.Client.raw_call = utils.create_raw_call()
 
-function core_v1.Client:call(method, path, body)
-  return self.client_:call(method, self.api_..path, body)
-end
+local nodes = "nodes"
+core_v1.Client.nodelist = utils.create_get_list(nodes)
+core_v1.Client.nodes = utils.create_get_all(nodes)
+core_v1.Client.node = utils.create_get_single(nodes)
+core_v1.Client.node_status = utils.create_get_single_status(nodes)
 
-function core_v1.Client:namespacelist()
-  return self:call("GET", "namespaces")
-end
+local namespaces = "namespaces"
+core_v1.Client.namespacelist = utils.create_get_list(namespaces)
+core_v1.Client.namespaces = utils.create_get_all(namespaces)
+core_v1.Client.namespace = utils.create_get_single(namespaces)
+core_v1.Client.namespace_status = utils.create_get_single_status(namespaces)
 
-function core_v1.Client:namespaces()
-  local namespaces = self:call("GET", "namespaces")
-  return fun.iter(namespaces.items)
-    :map(function(v) return obj.APIObject:new(v) end)
-    :totable()
-end
-
-function core_v1.Client:namespace(name)
-  local ns = self:call("GET", "namespaces/"..name)
-  return obj.APIObject:new(ns)
+local pods = "pods"
+core_v1.Client.podlist = utils.create_get_list_ns(pods)
+core_v1.Client.pods = utils.create_get_all_ns(pods)
+core_v1.Client.pod = utils.create_get_single_ns(pods)
+core_v1.Client.pod_status = utils.create_get_single_status_ns(pods)
+core_v1.Client.pod_logs = function(self, ns, name, args)
+  local path = string.format("namespaces/%s/%s/%s/log", ns, pods, name)
+  return self:raw_call("GET", path, nil, args)
 end
 
 return core_v1
