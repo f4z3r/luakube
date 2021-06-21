15dcdf7aa4702ad0c89dc1292b45a90e4af7eda3
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-06-21 15:17:56 +0200

    added mock client tests for pods, services, secrets, and configmaps

--- spec/kube/api_core_v1_spec.lua
+++ spec/kube/api_core_v1_spec.lua
@@ -158,6 +158,438 @@ describe("Core V1 ", function()
         assert.are.same(expected, info.body)
       end)
     end)
+
+    describe("inspecting pods", function()
+      it("should be able to return all", function()
+        local _, info = client:pods():get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/pods")
+      end)
+
+      it("should be able to return a specific one", function()
+        local _, info = client:pods("demo"):get("my-pod")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/pods/my-pod")
+      end)
+
+      it("should be able to return the status of a specific one", function()
+        local _, info = client:pods("demo"):status("my-pod")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/pods/my-pod/status")
+      end)
+
+      it("should be able to return all in list", function()
+        local _, info = client:pods():list()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/pods")
+      end)
+
+      it("should be able to return all in the kube-system namespace", function()
+        local _, info = client:pods("kube-system"):get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/pods")
+      end)
+
+      it("should be able to get logs of a pod", function()
+        local _, info = client:logs("kube-system", "coredns", {tailLines = 25})
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/pods/coredns/log?tailLines=25")
+      end)
+
+      it("should be able to update one", function()
+        local pod = {
+          metadata = {
+            name = "demo",
+            namespace = "kube-system"
+          },
+          spec = {
+            containers = {
+              {
+                name = "demo",
+                image = "busybox",
+              },
+            }
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "Pod",
+          metadata = {
+            name = "demo",
+            namespace = "kube-system"
+          },
+          spec = {
+            containers = {
+              {
+                name = "demo",
+                image = "busybox",
+              },
+            }
+          }
+        }
+        local _, info = client:pods("kube-system"):update(pod)
+        assert.are.equal("PUT", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/pods/demo")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to delete one", function()
+        local _, info = client:pods("kube-system"):delete("coredns")
+        assert.are.equal("DELETE", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/pods/coredns")
+      end)
+
+      it("should be able to create one", function()
+        local pod_yaml = [[
+metadata:
+  name: luakube-test-pod
+  labels:
+    luakube: forever
+spec:
+  containers:
+  - name: nginx
+    image: nginx:1.14.2
+    ports:
+    - containerPort: 80]]
+        local expected = {
+          apiVersion = "v1",
+          kind = "Pod",
+          metadata = {
+            name = "luakube-test-pod",
+            labels = {
+              luakube = "forever",
+            },
+          },
+          spec = {
+            containers = {
+              {
+                name = "nginx",
+                image = "nginx:1.14.2",
+                ports = {
+                  {
+                    containerPort = 80
+                  },
+                }
+              },
+            }
+          }
+        }
+        local _, info = client:pods("demo"):create(pod_yaml)
+        assert.are.equal("POST", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/pods")
+        assert.are.same(expected, info.body)
+      end)
+    end)
+
+    describe("inspecting services", function()
+      it("should be able to return all", function()
+        local _, info = client:services():get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/services")
+      end)
+
+      it("should be able to return a specific one", function()
+        local _, info = client:services("kube-system"):get("kube-dns")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/services/kube-dns")
+      end)
+
+      it("should be able to return the status of a specific one", function()
+        local _, info = client:services("kube-system"):status("kube-dns")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/services/kube-dns/status")
+      end)
+
+      it("should be able to return all in list", function()
+        local _, info = client:services():list()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/services")
+      end)
+
+      it("should be able to return all in the kube-system namespace", function()
+        local _, info = client:services("kube-system"):get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/services")
+      end)
+
+      it("should be able to update one", function()
+        local svc_obj = {
+          metadata = {
+            name = "demo-svc-test",
+            namespace = "demo",
+          },
+          spec = {
+            type = "ClusterIP",
+            ports = {
+              {
+                port = 443,
+                name = "https",
+                protocol = "TCP",
+              },
+            }
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "Service",
+          metadata = {
+            name = "demo-svc-test",
+            namespace = "demo",
+          },
+          spec = {
+            type = "ClusterIP",
+            ports = {
+              {
+                port = 443,
+                name = "https",
+                protocol = "TCP",
+              },
+            }
+          }
+        }
+        local _, info = client:services("demo"):update(svc_obj)
+        assert.are.equal("PUT", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/services/demo-svc-test")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to create one", function()
+        local svc_obj = {
+          metadata = {
+            name = "demo-svc-test",
+            namespace = "demo",
+          },
+          spec = {
+            type = "ClusterIP",
+            ports = {
+              {
+                port = 443,
+                name = "https",
+                protocol = "TCP",
+              },
+            }
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "Service",
+          metadata = {
+            name = "demo-svc-test",
+            namespace = "demo",
+          },
+          spec = {
+            type = "ClusterIP",
+            ports = {
+              {
+                port = 443,
+                name = "https",
+                protocol = "TCP",
+              },
+            }
+          }
+        }
+        local _, info = client:services("demo"):create(svc_obj)
+        assert.are.equal("POST", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/services")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to delete one", function()
+        local _, info = client:services("demo"):delete("svc")
+        assert.are.equal("DELETE", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/services/svc")
+      end)
+    end)
+
+    describe("inspecting configmaps", function()
+      it("should be able to return all", function()
+        local _, info = client:configmaps():get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/configmaps")
+      end)
+
+      it("should be able to return a specific one", function()
+        local _, info = client:configmaps("kube-system"):get("coredns")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/configmaps/coredns")
+      end)
+
+      it("should not have a status", function()
+        assert.is_nil(client:configmaps("kube-system").status)
+        assert.is_nil(client:configmaps("kube-system").update_status)
+      end)
+
+      it("should be able to return all in list", function()
+        local _, info = client:configmaps():list()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/configmaps")
+      end)
+
+      it("should be able to return all in the kube-system namespace", function()
+        local _, info = client:configmaps("kube-system"):get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/configmaps")
+      end)
+
+      it("should be able to update one", function()
+        local cm_obj = {
+          metadata = {
+            name = "demo-cm-test",
+            namespace = "demo",
+          },
+          data = {
+            url = "hello.world",
+            username = "whoami",
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "ConfigMap",
+          metadata = {
+            name = "demo-cm-test",
+            namespace = "demo",
+          },
+          data = {
+            url = "hello.world",
+            username = "whoami",
+          }
+        }
+        local _, info = client:configmaps("demo"):update(cm_obj)
+        assert.are.equal("PUT", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/configmaps/demo-cm-test")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to create one", function()
+        local cm_obj = {
+          metadata = {
+            name = "demo-cm-test",
+            namespace = "demo",
+          },
+          data = {
+            url = "hello.world",
+            username = "whoami",
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "ConfigMap",
+          metadata = {
+            name = "demo-cm-test",
+            namespace = "demo",
+          },
+          data = {
+            url = "hello.world",
+            username = "whoami",
+          }
+        }
+        local _, info = client:configmaps("demo"):create(cm_obj)
+        assert.are.equal("POST", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/configmaps")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to delete one", function()
+        local _, info = client:configmaps("demo"):delete("config")
+        assert.are.equal("DELETE", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/configmaps/config")
+      end)
+    end)
+
+    describe("inspecting secrets", function()
+      it("should be able to return all", function()
+        local _, info = client:secrets():get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/secrets")
+      end)
+
+      it("should be able to return a specific one", function()
+        local _, info = client:secrets("kube-system"):get("k3s-serving")
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/secrets/k3s-serving")
+      end)
+
+      it("should not have a status", function()
+        assert.is_nil(client:secrets("kube-system").status)
+        assert.is_nil(client:secrets("kube-system").update_status)
+      end)
+
+      it("should be able to return all in list", function()
+        local _, info = client:secrets():list()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/secrets")
+      end)
+
+      it("should be able to return all in the kube-system namespace", function()
+        local _, info = client:secrets("kube-system"):get()
+        assert.are.equal("GET", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/kube-system/secrets")
+      end)
+
+      it("should be able to update one", function()
+        local sec_obj = {
+          metadata = {
+            name = "demo-sec-test",
+            namespace = "demo",
+          },
+          type = "Opaque",
+          data = {
+            password = "c2VjcmV0"
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "Secret",
+          metadata = {
+            name = "demo-sec-test",
+            namespace = "demo",
+          },
+          type = "Opaque",
+          data = {
+            password = "c2VjcmV0"
+          }
+        }
+        local _, info = client:secrets("demo"):update(sec_obj)
+        assert.are.equal("PUT", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/secrets/demo-sec-test")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to create one", function()
+        local sec_obj = {
+          metadata = {
+            name = "demo-sec-test",
+            namespace = "demo",
+          },
+          type = "Opaque",
+          data = {
+            password = "c2VjcmV0"
+          }
+        }
+        local expected = {
+          apiVersion = "v1",
+          kind = "Secret",
+          metadata = {
+            name = "demo-sec-test",
+            namespace = "demo",
+          },
+          type = "Opaque",
+          data = {
+            password = "c2VjcmV0"
+          }
+        }
+        local _, info = client:secrets("demo"):create(sec_obj)
+        assert.are.equal("POST", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/secrets")
+        assert.are.same(expected, info.body)
+      end)
+
+      it("should be able to delete one", function()
+        local _, info = client:secrets("demo"):delete("sec")
+        assert.are.equal("DELETE", info.method)
+        assert.is.ending_with(info.url, "/api/v1/namespaces/demo/secrets/sec")
+      end)
+    end)
   end)
 end)
 
