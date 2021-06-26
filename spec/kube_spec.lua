448ac87c909ff0ef21fe3dadda824bda4dd738c9
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2021-06-26 19:10:07 +0200

    prepared release 0.1.0-0

--- spec/kube_spec.lua
+++ spec/kube_spec.lua
@@ -1,9 +1,18 @@
+#!/usr/bin/env lua
+
+--[[
+Author: Jakob Beckmann <beckmann_jakob@hotmail.fr>
+Description:
+ Test the base module.
+]]--
+
+local utils = require "spec.utils"
 local kube = require "kube"
 
 describe("Kube module", function()
   describe("should be tested", function()
-    it("should work", function()
-      assert.are.equal("hello", kube.test())
+    it("should return a version", function()
+      assert.is.starting_with(kube.version(), "0.1.0")
     end)
   end)
 end)
